53a08de8deb0dd321f25d11ceb0b02daa7e92f92
VinVaz <viniciusvazpro@gmail.com>
2018-04-06 21:48:17 -0300

    creates the basic structure of the program

--- urlShortenerMicroservice.js
+++ urlShortenerMicroservice.js
@@ -2,4 +2,16 @@ var http = require('http');
 var port = process.env.PORT || 8080;
 
   http.createServer(function(req, res){
+	  var originalUrl = req.url.slice(1);
+	  //shortUrl must come from a database
+	  var shortUrl = "";
+	  //a validation must be created to accept only real url
+	  var myMessage = {
+		original_url: originalUrl,
+        short_url:	shortUrl	
+	  }
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.write(JSON.stringify(originalUrl));
+	  res.end();
+	  
   }).listen(port);
