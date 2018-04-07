b33c9c85364f88c446fb4feaec190f0c24a328ca
VinVaz <viniciusvazpro@gmail.com>
2018-04-07 01:05:58 -0300

    basic structure of the program without the database interface plugged

--- urlShortenerMicroservice.js
+++ urlShortenerMicroservice.js
@@ -3,29 +3,55 @@ var port = process.env.PORT || 8080;
 
 	http.createServer(function(req, res){
 		
-		const originalUrl = req.url.slice(1);
-		//password comes from the database and it is stored there along with 
-		//the original url address when the last is stored  for first time
-		var password = "0950395";
-		var myHost = req.headers.host;
-		const shortUrl = `http://${myHost}/${password}`;
-
-		//validates the original url and creates a message with the response
-		var validation = /http:[/][/]www[.][a-z]+[.]com/i;
-		var isUrlValid = validation.test(originalUrl);
+		const myPath = req.url.slice(1);
+		const myHost = req.headers.host;
+		
 		var myMessage = {};
-		if(isUrlValid){
-			myMessage = {
-				original_url: originalUrl,
-				short_url:	shortUrl	
+		var isOriginalUrlStored = false;
+		var isPasswordStored = false;
+		
+		//checks if the request is an original url or 
+		//a path of some short url
+		var urlValidation = /http:[/][/]www[.][a-z]+[.]com/i;
+		var passwordValidation = /\d+/;
+		var isUrlValid = urlValidation.test(myPath);
+		var isPasswordValid = passwordValidation.test(myPath);
+		
+		if(isPasswordValid){
+			if(isPasswordStored){
+				//open the correspondent address
+				//instead of retriving the response
+			}
+			else{
+				myMessage = {
+					ERROR: "Invalid short URL address"
+				}
 			}
 		}
 		else{
-			myMessage = {
-				ERROR: "Invalid URL"
+			if(isUrlValid){
+				if(!isOriginalUrlStored){
+					//stores the original url on the database
+					//gets the id of that location on the database
+					//stores a password based on the id's value
+				}
+				
+				//recover the password
+				//example of password:
+				var password = "0950395";
+				const shortUrl = `http://${myHost}/${password}`;
+				
+				myMessage = {
+					original_url: myPath,
+					short_url:	shortUrl	
+				}
 			}
+			else{
+				myMessage = {
+					ERROR: "Invalid URL address"
+				}
+			}	
 		}
-	  
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.write(JSON.stringify(myMessage));
 		res.end();
