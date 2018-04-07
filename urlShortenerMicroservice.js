d8eaf31f6e76b27ee8578a723171e2c2270aa2d2
VinVaz <viniciusvazpro@gmail.com>
2018-04-06 22:23:47 -0300

    update

--- urlShortenerMicroservice.js
+++ urlShortenerMicroservice.js
@@ -5,7 +5,11 @@ var port = process.env.PORT || 8080;
 		var originalUrl = req.url.slice(1);
 		
 		//shortUrl must come from a database
+		//the database will store original url with some
+		//id number that can give to this program some 
+		//type of code to generate the new url address
 		var shortUrl = "";
+	
 		
 		//validates the original url and creates a message with the response
 		var validation = /http:[/][/]www[.][a-z]+[.]com/i;
@@ -24,7 +28,7 @@ var port = process.env.PORT || 8080;
 		}
 	  
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write(JSON.stringify(myMessage));
+		res.write(JSON.stringify(req.url));
 		res.end();
 	  
 	}).listen(port);
